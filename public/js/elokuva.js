9d12814f03cd7833e7889196495cd4a5a67a2124
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2019-12-14 15:41:54 +0200

    dummyrajapinta valmis

--- public/js/elokuva.js
+++ public/js/elokuva.js
@@ -1,11 +1,43 @@
+haeArvosteludata();
+
+async function haeArvosteludata(){
+  const response = await fetch('api/arvostelut');
+  const data = await response.json();
+  console.log(data);
+  console.log(data[0].arvostelija);
+  taytaArvostelutaulukko(data);
+}
+
+function taytaArvostelutaulukko(data) {
+  // Find a <table> element with id="myTable":
+  var table = document.getElementById("arvostelutaulukko");
+
+  for (var i = 0; i < data.length; i++) {
+
+    // Create an empty <tr> element and add it to the 1st position of the table:
+    var row = table.insertRow(i + 1);
+
+    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+
+    // Add some text to the new cells:
+    cell1.innerHTML = data[i].leffa;
+    cell2.innerHTML = data[i].arvostelu;
+    cell3.innerHTML = data[i].arvostelija;
+  }
+}
+
+
 function haeElokuva(){
 
-   haeData();
+   haeElokuvaData();
 
 }
 
 
-async function haeData(){
+async function haeElokuvaData(){
 
   var leffan_nimi = document.getElementById("elokuva").value;
   var leffan_vuosi = document.getElementById("vuosi").value;
@@ -21,4 +53,28 @@ async function haeData(){
   var poster = document.getElementById("elokuvan_juliste");
   poster.src = data.Poster;
 
+}
+
+function arvosteleElokuva(){
+  var leffa = document.getElementById("elokuva").value;
+  var arvostelu = document.getElementById("arvostelu").value;
+  var arvostelija = document.getElementById("arvostelija").value;
+
+  const data = {leffa, arvostelu, arvostelija};
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(data)
+  };
+
+  fetch('/api/arvostelu', options).then(function(response) {
+      console.log(response)
+      if(response.status == 200){
+        console.log("Push OK!")
+      }
+    }, function(error){
+      console.log(error.message);
+    });
 }
--- server.js
+++ server.js
@@ -6,3 +6,31 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("kuuntelen porttia " + port));
 
 app.use(express.static("public"));
+
+app.use(express.json({limit: '1mb'}));
+
+
+const arvostelut = [
+    {
+        "leffa": "Titanic",
+        "arvostelu": "Klassikkokamaa",
+        "arvostelija": "Suvi"
+    },
+    {
+        "leffa": "Star Wars",
+        "arvostelu": "Silmäkarkkia",
+        "arvostelija": "Suvi 2"
+    }
+]
+
+app.get('/api/arvostelut', function (request, response) {
+  response.send(arvostelut);
+})
+
+app.post('/api/arvostelu', function (request, response) {
+  console.log("Leffan arvostelu");
+  console.log(request.body);
+  arvostelut.push(request.body);
+  console.log(arvostelut);
+  response.send(200);
+})
